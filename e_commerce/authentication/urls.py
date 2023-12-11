2b4c846214ae5c25dabafab4b37a8507722a3204
Mustafa Hussain <51909599+chemical-son@users.noreply.github.com>
2023-12-11 08:36:20 +0200

    adding authentication
    -Login
    -Logout
    -Registration

--- e_commerce/authentication/urls.py
+++ e_commerce/authentication/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register, name='register'),
+    path('welcome/', views.welcome, name='welcome'),
 ]
--- e_commerce/authentication/views.py
+++ e_commerce/authentication/views.py
@@ -1,18 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # register new users
 # /auth/register/ :POST
 def register(request):
-    return render(request, 'authentication/register.html', {})
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password"]
+        password2 = request.POST["confirm_password"]
+
+        if username and email and password1 and password2:
+            # check if passwords match each other
+            if password1 != password2:
+                messages.error(request, "password dosn't match!")
+
+            try:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password1
+                )
+
+            except:
+                messages.error(request, "try defferant username or email!")
+
+            if user is not None:
+                login(request, user)
+                return redirect("/auth/welcome/")
+
+    return render(request, "authentication/register.html", {})
+
 
 # login user to access the system
 # /auth/login/ :POST
 def login_user(request):
-    return render(request, 'authentication/login.html', {})
+    if request.user.is_authenticated:
+        return redirect("/shop/")
+
+    if request.method == "POST":
+        username = request.POST["username"].lower()
+        password = request.POST["password"]
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exits")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/shop/")
+        else:
+            messages.error(request, "Usernaem or password does not correct")
+
+    return render(request, "authentication/login.html", {})
 
 
 # logout user
 # /auth/logout/ :POST
 def logout_user(request):
-    return render(request, 'authentication/login.html', {})
+    logout(request)
 
+    return redirect("/shop/")
+
+
+# welcome page
+# /auth/welcome/ :GET
+def welcome(request):
+    return render(request, "authentication/welcome.html", {})
